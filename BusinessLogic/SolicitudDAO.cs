31944453b5ef81f5401ef31dd4982ec9f9603621
José Arturo García López <jarturogl05@gmail.com>
2019-04-12 17:47:22 -0500

    SolicitudDAo

--- BusinessLogic/SolicitudDAO.cs
+++ BusinessLogic/SolicitudDAO.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,7 +19,13 @@ namespace BusinessLogic
                 using (SqlCommand command = new SqlCommand("INSERT INTO dbo.Solicitud VALUES (@Opcion1, @Opcion2, @opcion3, " +
                     "@FechaSolicitud, @EstadoSolicitud, @Alumno)", connection))
                 {
-                    command.Parameters.Add(new SqlParameter("@opcion1"))
+                    command.Parameters.Add(new SqlParameter("@opcion1", solicitud.Proyectos[0].NombreProyecto));
+                    command.Parameters.Add(new SqlParameter("@opcion2", solicitud.Proyectos[1].NombreProyecto));
+                    command.Parameters.Add(new SqlParameter("@opcion3", solicitud.Proyectos[2].NombreProyecto));
+                    command.Parameters.Add(new SqlParameter("@FechaSolicitud", solicitud.FechaSolicitud));
+                    command.Parameters.Add(new SqlParameter("@EstadoSolicitud", solicitud.EstadoSolicitud));
+                    command.Parameters.Add(new SqlParameter("@Alumno", solicitud.Alumno.NombreAlumno));
+                    command.ExecuteNonQuery();
                 }
                 connection.Close();
             }
