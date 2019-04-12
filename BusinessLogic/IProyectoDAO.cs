3d34355179fa49bbff3a3085344f9111e1401310
José Arturo García López <jarturogl05@gmail.com>
2019-04-12 18:21:18 -0500

    proyecto dao

--- BusinessLogic/IProyectoDAO.cs
+++ BusinessLogic/IProyectoDAO.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +10,7 @@ namespace BusinessLogic
     {
        List<Proyecto> GetProyectos();
        void AddProyecto(Proyecto proyecto);
-
+        Proyecto GetProyectoByID(String idToSearch);
        
 
     
