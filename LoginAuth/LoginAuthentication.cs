72468a75fee11cb6b8c1f5d0481456841af6e1ce
David <davisbd100@gmail.com>
2019-06-08 13:37:37 -0500

    Se agrego el modulo de Login sin probar

---
+++ LoginAuth/LoginAuthentication.cs
@@ -0,0 +1,56 @@
+﻿using DataBase;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginAuth
+{
+    public class LoginAuthentication
+    {
+        
+        public enum validationResult
+        {
+            Success,
+            UnknownFail,
+            UserOrPasswordIncorrect,
+            UserIncorrect,
+            PasswordIncorrect
+        }
+
+        public validationResult CredentialsAuthentication(String user, String pass)
+        {
+            validationResult result = validationResult.UnknownFail;
+            DbConnection dbConnection = new DbConnection();
+            String tipoString = "";
+            using (SqlConnection connection = dbConnection.GetConnection())
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand("SELECT [Tipo_Usuario] FROM [dbo].[Usuarios] WHERE Usuario = @usuario AND Contraseña = @password"))
+                {
+                    command.Parameters.Add(new SqlParameter("@usuario", user));
+                    command.Parameters.Add(new SqlParameter("password", pass));
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        tipoString = reader["Tipo_Usuario"].ToString();
+                    }
+                    
+                    if (String.IsNullOrEmpty(tipoString))
+                    {
+                        result = validationResult.UserOrPasswordIncorrect;
+                    }
+                    else
+                    {
+                        result = validationResult.Success;
+                    }
+
+                }
+
+            }
+            return result;    
+        }
+    }
+}
