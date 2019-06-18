593d88f60982dcb0e2c7cf6b06a904547924ee2d
José Arturo <jarturogl05@gmail.com>
2019-06-17 22:09:28 -0500

    Funcionalidad de crear usuario al agregar coordinador

--- BusinessLogic/Usuario.cs
+++ BusinessLogic/Usuario.cs
@@ -14,5 +14,20 @@ namespace BusinessLogic
         public DateTime RegisterDate { get; set; }
         public String Name { get; set; }
         public String Email { get; set; }
+
+        public Usuario(string UserName, string Password, string UserType, DateTime RegisterDate, string Name, string Email)
+        {
+            this.UserName = UserName;
+            this.Password = Password;
+            this.UserType = UserType;
+            this.RegisterDate = RegisterDate;
+            this.Name = Name;
+            this.Email = Email;
+        }
+
+        public Usuario()
+        {
+
+        }
     }
 }
